85ff94ea85ba627af7cbe0ec0bf51ded0fd9197e
Arunan Thiviyanathan <arunankogulan@gmail.com>
2021-03-07 01:21:40 -0600

    Fixed Manifest

---
+++ articles.js
@@ -0,0 +1,6 @@
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('6aa91d34573842b88c17af61f04bbbd8');
+const topics = ["Gun Control", "Abortion", "Religious Freedom", "Animal Rights", "Vaccines", "Privacy", "Capitalism", "Climate Change", "Evolution", "Marijuana", "Capital Punishment", "Marriage", "Immigration", "Trump", "Opioid", "Transgender", "Universal Basic Income", "White Supremacy", "Green New Deal", "BLM", "Cancel Culture", "College", "Israel", "AI", "Atheist", "Social Security", "Addiction", "Fracking", "Nuclear", "Unions", "Insurance", "Agriculture", "Women", "Cryptocurrency", "Biofuel", "Medicine", "Speech", "COVID", "Mexican Border", "Net Neutrality", "Extremism", "Terrorism", "Paris Climate Accord", "Misinformation", "Voter Fraud", "Tarriff", "Supreme Court", "Stimulus", "Police", "Autopilot", "Sex", "Stem Cells", "Mental Health", "Slavery", "Death Penalty", "Vegetarian", "Vaping", "Cuba", "Athletes", "Brexit", "Feminism", "Gentrification", "Kashmir", "Drug", "Patriot Act", "Islam", "Buddhism", "AIDS", "Gene Editing", "ADHD", "Dyslexic", "Eugenics", "Euthanasia", "LGBTQ", "Gay", "Wildfire", "Hurricane", "Conservative", "Liberal", "Socialism", "Oil", "Pollution", "Tax", "Piracy"];
+for (let topic of topics) {
+    console.log(topic);
+}
--- popup.js
+++ popup.js
@@ -1,14 +1,17 @@
+function getText(){
+    return document.body.innerText
+}
 var totalCount = 0;
 document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('checkPage');
     checkPageButton.addEventListener('click', function() {
-        totalCount+=1;
-        ///Gets the text from the page
+        totalCount += 1;
         chrome.tabs.executeScript(null, {
             code: `document.all[0].innerText`,
-            allFrames: false,
-            runAt: 'document_start',
+            allFrames: false, // this is the default
+            runAt: 'document_start', // default is document_idle. See https://stackoverflow.com/q/42509273 for more details.
         }, function(results) {
+            // results.length must be 1
             var result = results[0];
             console.log(result);
         });
