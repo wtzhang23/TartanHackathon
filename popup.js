622357bf4cd57ad904e12e8a24f9cc0b1cdc2b41
Arunan Thiviyanathan <arunankogulan@gmail.com>
2021-03-07 10:32:47 -0600

    javascript is dum

--- popup.js
+++ popup.js
@@ -4,12 +4,26 @@ const url = "https://tartanhackathon.uc.r.appspot.com/recommend?num=10";
 function getText() {
     return document.body.innerText
 }
-localStorage.setItem('counter', 0);
+if ("counter" in localStorage) {
+    let today = new Date();
+    if (localStorage['lastUpdate'] != null) {
+        if (parseInt(localStorage['lastUpdate']) != today.getDate()) {
+            console.log("DEBUG");
+            localStorage['lastUpdate'] =  today.getDate();
+            localStorage['counter'] = 0;
+        }
+    }
+} else {
+    localStorage['counter'] = 0;
+    localStorage['lastUpdate'] =  new Date().getDate();
+}
+document.getElementById("progress").src = `img/${Math.min(5, localStorage['counter'])}.png`;
 document.addEventListener('DOMContentLoaded', function () {
     var checkPageButton = document.getElementById('checkPage');
     checkPageButton.addEventListener('click', function (e) {
+        var count = parseInt(localStorage['counter']);
+        localStorage.setItem('counter', count + 1);
         e.preventDefault();
-
         req.onload = function() {
             var articles = JSON.parse(req.responseText)
             //jswin = window.open("", "jswin", "width=550,height=450");
@@ -18,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 chrome.tabs.create({'url': article, 'active': false}, function(tab){})
             }
         }
-        localStorage.setItem('counter', parseInt(localStorage.getItem('counter')) + 1);
+        
         chrome.tabs.executeScript(null, {
             code: `document.all[0].innerText`,
             allFrames: false, // this is the default
@@ -39,7 +53,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
         totalCount = parseInt(localStorage.getItem('counter'))
         if (totalCount <= 5) {
-            document.getElementById("progress").src = `img/${totalCount}.png`;
+            document.getElementById("progress").src = `img/${Math.min(5, localStorage['counter'])}.png`;
         }
     }, false);
 }, false);
